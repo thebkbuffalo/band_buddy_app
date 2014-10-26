1617b3b385eb79e63948d14f645be8312f76ef9f
thebkbuffalo <evan.m.berg@gmail.com>
2014-10-26 19:59:25 -0400

    starting on questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -10,6 +10,8 @@ class QuestionsController < ApplicationController
   # GET /questions/1
   # GET /questions/1.json
   def show
+    @questions = Question.all
+    @answers = Answer.al
   end
 
   # GET /questions/new
