ad8b0925e5ad814dae43a028d8a98cb6718f1616
thebkbuffalo <evan.m.berg@gmail.com>
2014-10-26 23:51:12 -0400

    routes up, now just gotta figure out how to render everything

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,7 +4,7 @@ class QuestionsController < ApplicationController
   # GET /questions
   # GET /questions.json
   def index
-    @questions = Question
+    @questions = Question.all
   end
 
   # GET /questions/1
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,10 @@ Rails.application.routes.draw do
 
   resources :questions, only: [:index, :show]
 
-  resources :users
+  resources :users do 
+    resources :questions, only: [:index, :show]
+    resources :answers, only: [:index, :show]
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
