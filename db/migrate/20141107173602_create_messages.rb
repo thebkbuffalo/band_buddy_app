43fea4c04218742b95358ec208859a234f18aa21
thebkbuffalo <evan.m.berg@gmail.com>
2014-11-07 12:41:02 -0500

    beginning stages of messanger

--- db/migrate/20141107173602_create_messages.rb
+++ db/migrate/20141107173602_create_messages.rb
@@ -1,8 +1,10 @@
-class Messages < ActiveRecord::Migration
+class CreateMessages < ActiveRecord::Migration
   def change
     create_table :messages do |t|
       t.text :message
-      t.integer :receiver_id
       t.integer :sender_id
+      t.integer :receiver_id
+      t.timestamps
+    end
   end
 end
