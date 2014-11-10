a8b24553aa74e47cc22f4fc84590e85d05f20103
thebkbuffalo <evan.m.berg@gmail.com>
2014-11-10 12:45:06 -0500

    added messages controller and created merge file for messages

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,25 @@
+class MessagesController < ApplicationController
+
+# 	def show
+# 		if logged_in?
+# 			@messages = Message.where("(sender_id = ? OR receiver_id=?) AND (sender_id=? OR receiver_id=?)", params[:id],params[:id],session[:id],session[:id])
+# 			render json: @messages.to_json(:include => [{:sender  => {:only => [:first_name,:last_name,:picture_url]}},{:receiver =>{:only=>[:first_name,:last_name,:picture_url]}}])
+# 		end
+# 	end
+#
+#
+#  # TODO - create notifactions table to go along with notifications for messanger service
+# 	def create
+# 		if logged_in?
+# 			match = Match.where('(first_user_id = ? AND second_user_id = ?) OR (first_user_id = ? AND second_user_id = ?)',session[:id],params[:receiver_id],params[:receiver_id],session[:id])[0]
+# 			match.touch
+# 			Notification.create(user_id:params[:receiver_id],seen:false)
+#
+# 			@message = Message.create(sender_id:session[:id],receiver_id:params[:receiver_id],message:params[:message])
+# 			render json: @message
+#
+#
+# 		end
+# 	end
+
+end
